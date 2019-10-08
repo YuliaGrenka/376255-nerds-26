a23e1fe3958f336cb6a4b001bd9ce8c295b117e5
Grenka <yulia.grenka@gmail.com>
2019-10-08 23:57:23 +0300

    Добавила скрипт для всплывающего окна

---
+++ js/index.js
@@ -0,0 +1,70 @@
+var writeLink = document.querySelector(".button-write");
+var writeOverlay = document.querySelector(".modal-overlay");
+var writePopup = document.querySelector(".modal");
+var writeClose = writePopup.querySelector(".modal-close");
+var writeLogin = writePopup.querySelector("[name=login]");
+var writeEmail = writePopup.querySelector("[name=email]");
+var writeMessage = writePopup.querySelector("[name=message]");
+var isStorageSupport = true;
+var storage = "";
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+var writeForm = writePopup.querySelector("form");
+
+writeLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  writePopup.classList.add("modal-show");
+  writePopup.classList.add("modal-animation");
+  writeOverlay.classList.add("modal-show");
+  if (storage) {
+    writeLogin.value = storage;
+    writeEmail.focus();
+  } else {
+    writeLogin.focus();  
+  }
+  
+});
+
+writeClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  writePopup.classList.remove("modal-show");
+  writePopup.classList.remove("modal-animation");
+  writeOverlay.classList.remove("modal-show");
+  writePopup.classList.remove("modal-error");
+});
+
+writeOverlay.addEventListener("click", function (evt) {
+  writePopup.classList.remove("modal-show");
+  writePopup.classList.remove("modal-animation");  
+  writeOverlay.classList.remove("modal-show");
+  writePopup.classList.remove("modal-error");
+});
+
+writeForm.addEventListener("submit", function (evt) {
+ 
+  if (!writeLogin.value || !writeEmail.value || !writeMessage.value) {
+    evt.preventDefault();
+    writePopup.classList.remove("modal-error");
+    writePopup.offsetWidth = writePopup.offsetWidth;
+    writePopup.classList.add("modal-error");  
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", writeLogin.value);
+    }
+  }  
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (writePopup.classList.contains("modal-show")){
+      writePopup.classList.remove("modal-show");
+      writePopup.classList.remove("modal-animation");
+      writeOverlay.classList.remove("modal-show");
+      writePopup.classList.remove("modal-error");
+    }
+  }
+});
