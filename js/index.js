5108531fbeb42ffe29d3e898fcbf4af6c6b7b328
Grenka <yulia.grenka@gmail.com>
2019-10-12 19:44:48 +0300

    Добавила минимизированные файлы стиля и скриптов. Поправила ссылку для номера телефона.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+var myMap;
 var writeLink = document.querySelector(".button-write");
 var writeOverlay = document.querySelector(".modal-overlay");
 var writePopup = document.querySelector(".modal");
@@ -13,6 +14,43 @@ try {
   isStorageSupport = false;
 }
 var writeForm = writePopup.querySelector("form");
+
+ymaps.ready(init);
+
+function init () {
+  // Создание экземпляра карты и его привязка к контейнеру с
+  // заданным id ("map").
+  myMap = new ymaps.Map($('.map')[0], {
+      // При инициализации карты обязательно нужно указать
+      // её центр и коэффициент масштабирования.
+      center: [59.939008, 30.321048], 
+      zoom: 17,
+      controls: []
+  }, {
+      searchControlProvider: 'yandex#search'
+  }),
+  MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+  ),
+
+  myPlacemark = new ymaps.Placemark([59.938657, 30.323273], {
+
+  }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/map-marker.png',
+      // Размеры метки.
+      iconImageSize: [231, 190],
+      iconImageOffset: [-59, -208]
+  });
+
+  myMap.geoObjects.add(myPlacemark); 
+
+}
+
+
 
 writeLink.addEventListener("click", function (evt) {
   evt.preventDefault();
